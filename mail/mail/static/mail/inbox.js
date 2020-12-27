bbf17f5b5345bd9acd19eb0d4a829c1a64e7d46b
VincVader <vincvader@mail.ru>
2020-12-27 15:10:53 +0300

    Cosmetic

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,8 +1,23 @@
 // Use buttons to toggle between views
-document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-document.querySelector('#compose').addEventListener('click', compose_email);
+
+const inbox = document.querySelector('#inbox');
+const sent = document.querySelector('#sent');
+const archived = document.querySelector('#archived');
+const compose = document.querySelector('#compose');
+
+const emailsV = document.querySelector('#emails-view');
+const composeV = document.querySelector('#compose-view');
+const composeR = document.querySelector('#compose-recipients');
+const composeS = document.querySelector('#compose-subject');
+const composeB = document.querySelector('#compose-body');
+
+
+
+
+inbox.addEventListener('click', () => load_mailbox('inbox'));
+sent.addEventListener('click', () => load_mailbox('sent'));
+archived.addEventListener('click', () => load_mailbox('archive'));
+compose.addEventListener('click', compose_email);
 
 // By default, load the inbox
 load_mailbox('inbox');
@@ -11,20 +26,23 @@ load_mailbox('inbox');
 function compose_email() {
 
 	// Show compose view and hide other views
-	document.querySelector('#emails-view').style.display = 'none';
-	document.querySelector('#compose-view').style.display = 'block';
+	emailsV.style.display = 'none';
+	composeV.style.display = 'block';
 
 	// Clear out composition fields
-	document.querySelector('#compose-recipients').value = '';
-	document.querySelector('#compose-subject').value = '';
-	document.querySelector('#compose-body').value = '';
+	composeR.value = '';
+	composeS.value = '';
+	composeB.value = '';
+	
 }
 
 function load_mailbox(mailbox) {
+
+
 	// Show the mailbox and hide other views
-	document.querySelector('#emails-view').style.display = 'block';
-	document.querySelector('#compose-view').style.display = 'none';
+	emailsV.style.display = 'block';
+	composeV.style.display = 'none';
 
 	// Show the mailbox name
-	document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+	emailsV.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
